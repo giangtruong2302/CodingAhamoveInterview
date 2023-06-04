21087ce522282d24ebd3ac748a90ad88d8ac1bf6
Truong Giang <giangtruong2302@gmail.com>
2023-06-04 11:48:59 +0700

    fix some bug to complete some feature todo app

--- src/component/Todo/Task/index.js
+++ src/component/Todo/Task/index.js
@@ -4,6 +4,7 @@ import classNames from "classnames";
 
 const Task = ({ title, content, key }) => {
   const [expanded, setExpanded] = useState(false);
+  const [showModal, setShowModal] = useState(false);
   const task = useStore((store) =>
     store.tasks.find((task) => task.title === title)
   );
@@ -12,6 +13,16 @@ const Task = ({ title, content, key }) => {
   const deleteTask = useStore((store) => store.deleteTask);
   const handleToggleExpand = () => {
     setExpanded(!expanded);
+  };
+  const handleDeleteTask = () => {
+    setShowModal(true);
+  };
+  const handleConfirmDelete = () => {
+    deleteTask(task.title, task.state);
+    setShowModal(false);
+  };
+  const handleCancelDelete = () => {
+    setShowModal(false);
   };
   return (
     <div
@@ -49,19 +60,8 @@ const Task = ({ title, content, key }) => {
           {task.content}
         </div>
       </div>
-      {/* {task.content.length > 50 && (
-        <div
-          className="text-[12px] cursor-pointer"
-          onClick={handleToggleExpand}
-        >
-          {expanded ? "Show less" : "Show more"}
-        </div>
-      )} */}
       <div className="flex justify-between mt-3">
-        <div
-          onClick={() => deleteTask(task.title, task.state)}
-          className="cursor-pointer"
-        >
+        <div onClick={handleDeleteTask} className="cursor-pointer">
           <img
             src={"/icons/trash-2.svg"}
             alt={task.title}
@@ -77,6 +77,30 @@ const Task = ({ title, content, key }) => {
           {task.state}
         </div>
       </div>
+      {showModal && (
+        <div className="absolute inset-0 flex justify-center items-center bg-opacity-30 bg-gray-950">
+          <div className="bg-white p-6 rounded-md shadow-lg w-[300px]">
+            <div className="text-[#000] font-bold">Confirm Delete</div>
+            <p className="text-[#000]">
+              Are you sure you want to delete this task?
+            </p>
+            <div className="flex justify-end mt-4">
+              <button
+                className="px-4 py-2 mr-2 rounded-md bg-red-400 text-white"
+                onClick={handleConfirmDelete}
+              >
+                Yes
+              </button>
+              <button
+                className="px-4 py-2 rounded-md bg-gray-300"
+                onClick={handleCancelDelete}
+              >
+                No
+              </button>
+            </div>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
